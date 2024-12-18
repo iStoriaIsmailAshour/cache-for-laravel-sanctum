e10289cad63abca2ad4654845e1bf1fb33ae0569
Ismail Ashour <ismail@istoria.app>
2024-12-18 17:26:45 +0200

    Ignore save last_used_at

--- src/PersonalAccessToken.php
+++ src/PersonalAccessToken.php
@@ -85,4 +85,14 @@ class PersonalAccessToken extends Sanctum
             )
         );
     }
+
+    public function save(array $options = [])
+    {
+        $changes = $this->getDirty();
+        // Check for 2 changed values because one is always the updated_at column
+        if (!array_key_exists('last_used_at', $changes) || count($changes) > 2) {
+            parent::save();
+        }
+        return false;
+    }
 }
